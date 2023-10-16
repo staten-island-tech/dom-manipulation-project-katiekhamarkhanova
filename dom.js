2760c994fc9aa38b4716f13d201268704b0e28bf
katie <113192783+katiekhamarkhanova@users.noreply.github.com>
2023-10-16 09:26:22 -0400

    dom

---
+++ dom.js
@@ -0,0 +1,12 @@
+const DOMSelectors = {
+    form: document.querySelector("#form"),
+    firstName: document.querySelector(".first-name"),
+    h2s: document.querySelectorAll("h2"),
+  };
+  DOMSelectors.form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    console.log(DOMSelectors.firstName.value);
+    DOMSelectors.h2s.forEach(
+      (el) => el.textContent = DOMSelectors.firstName.value
+    );
+  });
