8ba417936ace6d4fe594dd1b8d75044a915a2399
katie <113192783+katiekhamarkhanova@users.noreply.github.com>
2023-10-17 09:24:49 -0400

    a

--- dom.js
+++ dom.js
@@ -1,6 +1,10 @@
 const DOMSelectors = {
     form: document.querySelector("#form"),
     firstName: document.querySelector(".first-name"),
+    lastName: document.querySelector(".last-name"),
+    desc: document.querySelector(".description"),
+    h3s: document.querySelectorAll("h3"),
+    paragraphs: document.querySelectorAll("p"),
     h2s: document.querySelectorAll("h2"),
   };
   DOMSelectors.form.addEventListener("submit", function (event) {
@@ -8,5 +12,13 @@ const DOMSelectors = {
     console.log(DOMSelectors.firstName.value);
     DOMSelectors.h2s.forEach(
       (el) => el.textContent = DOMSelectors.firstName.value
+    );
+    console.log(DOMSelectors.lastName.value);
+    DOMSelectors.h3s.forEach(
+      (el) => el.textContent = DOMSelectors.lastName.value
+    );
+    console.log(DOMSelectors.desc.value);
+    DOMSelectors.paragraphs.forEach(
+      (el) => el.textContent = DOMSelectors.desc.value
     );
   });
